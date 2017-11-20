cce92464c633b556d8fa38e3a00dd2ebb92c816c
klh5 <klh5@aber.ac.uk>
2017-11-20 17:03:51 +0000

    Minor changes to variable names

--- removeOutliers.py
+++ removeOutliers.py
@@ -8,11 +8,14 @@ class RLMRemoveOutliers(object):
         
         self.T = 365
         self.pi_val = (2 * np.pi) / self.T
+        self.pi_val_change = None
     
     def clean_data(self, pixel_data, num_years):
         
         """Extracts the Band 2 and Band 5 data, which are used to build RLMs for detecting cloud and snow outliers"""
     
+        self.pi_val_change = (2 * np.pi) / (num_years * self.T)
+        
         # Extract list of dates
         julian_dates = pixel_data[:,0]
 
@@ -20,21 +23,21 @@ class RLMRemoveOutliers(object):
         band2_ref = pixel_data[:,1]
 
         # Model band 2 data
-        band2_coeffs = self.makeRLMModel(band2_ref, julian_dates, num_years)
+        band2_coeffs = self.makeRLMModel(band2_ref, julian_dates)
         
         # Find band 2 outliers
         band2_data = np.column_stack((julian_dates, band2_ref))
-        band2_outliers = self.removeBadPixels(band2_data, band2_coeffs, num_years, 2)
+        band2_outliers = self.removeBadPixels(band2_data, band2_coeffs, 2)
         
         # Extract band 5 data
         band5_ref = pixel_data[:,4]
         
         # Model band 5 data
-        band5_coeffs = self.makeRLMModel(band5_ref, julian_dates, num_years)
+        band5_coeffs = self.makeRLMModel(band5_ref, julian_dates)
         
         # Find band 5 outliers
         band5_data = np.column_stack((julian_dates, band5_ref))
-        band5_outliers = self.removeBadPixels(band5_data, band5_coeffs, num_years, 5)
+        band5_outliers = self.removeBadPixels(band5_data, band5_coeffs, 5)
     
         # Remove outliers from data
         pixel_data = np.delete(pixel_data, band2_outliers, axis=0)
@@ -43,13 +46,11 @@ class RLMRemoveOutliers(object):
         return pixel_data
     
 
-    def makeRLMModel(self, band_data, julian_dates, N):
+    def makeRLMModel(self, band_data, julian_dates):
         
         """Builds the model and stores the coefficients"""
 
         terms = []
-        
-        pi_val_change = (2 * np.pi) / (N * self.T)
 
         a0 = band_data / band_data
         terms.append(a0)
@@ -60,10 +61,10 @@ class RLMRemoveOutliers(object):
         b1i = np.sin(self.pi_val * julian_dates)
         terms.append(b1i)
     
-        a2i = np.cos(pi_val_change * julian_dates)
+        a2i = np.cos(self.pi_val_change * julian_dates)
         terms.append(a2i)
         
-        b2i = np.sin(pi_val_change * julian_dates)
+        b2i = np.sin(self.pi_val_change * julian_dates)
         terms.append(b2i)
         
         terms = np.array(terms).T
@@ -75,16 +76,15 @@ class RLMRemoveOutliers(object):
         
         return coeff_list
     
-    def removeBadPixels(self, band_data, coefficients, N, band_num):
+    def removeBadPixels(self, band_data, coefficients, band_num):
         
         """Goes through each observation and makes a list of outliers"""
         
-        pi_val_change = (2 * np.pi) / (N * self.T)
         outliers = []
     
         for index, row in enumerate(band_data):
 
-            predicted_value = coefficients[0] + (coefficients[1] * np.cos(self.pi_val * row[0])) + (coefficients[2] * np.sin(self.pi_val * row[0])) + (coefficients[3] * np.cos(pi_val_change * row[0])) + (coefficients[4] * np.sin(pi_val_change * row[0]))
+            predicted_value = coefficients[0] + (coefficients[1] * np.cos(self.pi_val * row[0])) + (coefficients[2] * np.sin(self.pi_val * row[0])) + (coefficients[3] * np.cos(self.pi_val_change * row[0])) + (coefficients[4] * np.sin(self.pi_val_change * row[0]))
             
             difference = row[1] - predicted_value
 

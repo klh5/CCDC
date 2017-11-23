18bb3584b30d1e704a49120719f0b0abd07d5861
klh5 <klh5@aber.ac.uk>
2017-11-23 17:46:25 +0000

    Updating RIRLS modelling to use pandas

--- removeOutliers.py
+++ removeOutliers.py
@@ -11,83 +11,44 @@ class RLMRemoveOutliers(object):
         self.T = 365
         self.pi_val = (2 * np.pi) / self.T
         self.pi_val_change = None
-        self.band2_coeffs = None
-        self.band4_coeffs = None
-        self.band5_coeffs = None
+        self.band2_model = None
+        self.band4_model = None
+        self.band5_model = None
     
     def clean_data(self, pixel_data, num_years):
         
         """Extracts the Band 2, band 4, and Band 5 data, which are used to build RLMs for detecting cloud and snow outliers"""
         self.pi_val_change = (2 * np.pi) / (num_years * self.T)
-        
-        # Extract list of dates
-        julian_dates = pixel_data[:,0]
-
-        # Extract band 2 data
-        band2_only = pixel_data[:,1]
-
-        # Extract band 4 data
-        band4_only = pixel_data[:,3]
-
-        # Extract band 5 data
-        band5_only = pixel_data[:,4]
 
         # Get coefficients for the three models
-        self.band2_coeffs = self.makeRLMModel(band2_only, julian_dates)
-        self.band4_coeffs = self.makeRLMModel(band4_only, julian_dates)
-        self.band5_coeffs = self.makeRLMModel(band5_only, julian_dates)
+        self.band2_model = self.makeRLMModel(pixel_data[['datetime', 'band_2']])
+        self.band4_model = self.makeRLMModel(pixel_data[['datetime', 'band_4']])
+        self.band5_model = self.makeRLMModel(pixel_data[['datetime', 'band_5']])
 
-        outlier_bands = np.column_stack((julian_dates, band2_only, band4_only, band5_only))
-        
-        test_fmodel = np.column_stack((julian_dates, band2_only))
-        p_test_fmodel = pd.DataFrame(data=test_fmodel[0:,0:], index=test_fmodel[:,0], columns=['julian_date', 'band2'])
+        #predictions = fcoeffs.predict()
+        #print(predictions, band2_only)
 
-        fmodel = smf.rlm('band2 ~ 1 + (np.cos(self.pi_val * julian_date) + np.sin(self.pi_val * julian_date)) + (np.cos(self.pi_val_change * julian_date)) + (np.sin(self.pi_val_change * julian_date))', p_test_fmodel)
+        #print(fcoeffs.params)
+        #print(self.band2_coeffs)
 
-        fcoeffs = fmodel.fit(maxiter=5)
-        predictions = fcoeffs.predict()
-        print(predictions, band2_only)
-
-        print(fcoeffs.params)
-        print(self.band2_coeffs)
-
-        outliers = self.removeBadPixels(outlier_bands)
+        #outliers = self.removeBadPixels(outlier_bands)
     
         # Remove outliers from data
-        pixel_data = np.delete(pixel_data, outliers, axis=0)
+        #pixel_data = np.delete(pixel_data, outliers, axis=0)
     
         return pixel_data
     
 
-    def makeRLMModel(self, band_data, julian_dates):
+    def makeRLMModel(self, band_data):
         
         """Builds the model and stores the coefficients"""
+        
+        band_data.columns = ['datetime', 'reflectance']
 
-        terms = []
-
-        a0 = band_data / band_data
-        terms.append(a0)
-
-        a1i = np.cos(self.pi_val * julian_dates)
-        terms.append(a1i)
-    
-        b1i = np.sin(self.pi_val * julian_dates)
-        terms.append(b1i)
-    
-        a2i = np.cos(self.pi_val_change * julian_dates)
-        terms.append(a2i)
+        rlm_model = smf.rlm('reflectance ~ 1 + (np.cos(self.pi_val * datetime) + np.sin(self.pi_val * datetime)) + (np.cos(self.pi_val_change * datetime)) + (np.sin(self.pi_val_change * datetime))', band_data)
+        rlm_result = rlm_model.fit(maxiter=5)
         
-        b2i = np.sin(self.pi_val_change * julian_dates)
-        terms.append(b2i)
-        
-        terms = np.array(terms).T
-            
-        rlm_model = sm.RLM(band_data, terms, M=sm.robust.norms.TukeyBiweight())
-        
-        # Paper suggests a maximum of 5 iterations
-        coeff_list = rlm_model.fit(maxiter=5).params
-        
-        return coeff_list
+        return rlm_result
     
     def removeBadPixels(self, band_data):
         

a5d7154ae5271d8d407a33197c5bff53b4d25ef0
klh5 <klh5@aber.ac.uk>
2018-01-29 10:51:12 +0000

    Adding script to generate change map from CSV files

--- change_maps/make_plot.py
+++ change_maps/make_plot.py
@@ -5,6 +5,7 @@ import sys
 import csv
 import xarray as xr
 import numpy as np
+import re
 
 def main():
 
@@ -26,7 +27,7 @@ def main():
                     sep_filename = change_file.replace('.', '_').split('_')
 
                     x_val = float(sep_filename[0])
-                    y_val = float(sep_filename[1])
+                    y_val = float(sep_filename[2])
 
                     csv_file_path = os.path.join(file_path, change_file)
 
@@ -38,15 +39,15 @@ def main():
                         row = {'x': x_val, 'y': y_val, 'num_changes': num_changes}
                         rows.append(row)
 
-    to_df = pd.DataFrame(rows).set_index(['x', 'y'])
+        to_df = pd.DataFrame(rows).set_index(['x', 'y'])
     
-    dataset = xr.Dataset.from_dataframe(to_df)
+        dataset = xr.Dataset.from_dataframe(to_df)
 
-    dataset.attrs['crs'] = 'PROJCS["WGS 84 / UTM zone 55N",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",147],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","32655"]]'
+        dataset.attrs['crs'] = 'PROJCS["WGS 84 / UTM zone 55N",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",147],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","32655"]]'
 
-    print(dataset)
+        print(dataset)
 
-    dataset.to_netcdf('saved_on_disk.nc', encoding={'num_changes': {'dtype': 'uint16', '_FillValue': 9999}})
+        dataset.to_netcdf('ls8_ls7.nc', encoding={'num_changes': {'dtype': 'uint16', '_FillValue': 9999}})
                      
                     
 if __name__ == "__main__":

182e5ec1dce7afb2182f6ba67c317036557f2a72
klh5 <klh5@aber.ac.uk>
2018-06-25 12:21:38 +0100

    make_plot.py now outputs both a NetCDF and a matplotlib plot

--- change_maps/make_plot.py
+++ change_maps/make_plot.py
@@ -1,55 +1,65 @@
+'''
+Takes the CSV files output from CCDC, and outputs a change intensity map.
+'''
+
 import pandas as pd
 import os
 import fnmatch
 import sys
 import csv
 import xarray as xr
-import numpy as np
-import re
 from osgeo import gdal
+from matplotlib import pyplot as plt
 
 def main():
 
-    
-	if(len(sys.argv) > 1):
-		file_path = sys.argv[1]
+    if(len(sys.argv) > 1):
+        file_path = sys.argv[1]
 
-		print(file_path)
+        print(file_path)
         
-		if(os.path.isdir(file_path)):
+        if(os.path.isdir(file_path)):
 
-			rows = []
+            rows = []
         
             # For every file in the directory
-			for change_file in os.listdir(file_path):
+            for change_file in os.listdir(file_path):
 
-				if fnmatch.fnmatch(change_file, '*.csv'): # Check if it's a CSV file
+                if fnmatch.fnmatch(change_file, '*.csv'): # Check if it's a CSV file
 
-					sep_filename = change_file.replace('.', '_').split('_')
+                    sep_filename = change_file.replace('.', '_').split('_')
 
-					x_val = float(sep_filename[0])
-					y_val = float(sep_filename[2])
+                    x_val = float(sep_filename[0])
+                    y_val = float(sep_filename[2])
 
-					csv_file_path = os.path.join(file_path, change_file)
+                    csv_file_path = os.path.join(file_path, change_file)
 
-					with open(csv_file_path, "r") as data_file:
-						file_reader = csv.reader(data_file)
+                    with open(csv_file_path, "r") as data_file: 
+                       
+                        file_reader = csv.reader(data_file)
 
-						num_changes = sum(1 for row in file_reader) - 1
-						num_changes = num_changes * 100 # Scale up
+                        num_changes = sum(1 for row in file_reader) - 1
                             
-						row = {'x': x_val, 'y': y_val, 'num_changes': num_changes}
-						rows.append(row)
+                        row = {'x': x_val, 'y': y_val, 'num_changes': num_changes}
+                        rows.append(row)
 
-		to_df = pd.DataFrame(rows).set_index(['y', 'x'])
+        to_df = pd.DataFrame(rows).set_index(['y', 'x'])
     
-		dataset = xr.Dataset.from_dataframe(to_df)
+        dataset = xr.Dataset.from_dataframe(to_df)
+        
+        img_name = "new_map.nc"
 
-		dataset.to_netcdf('new_map.nc', encoding={'num_changes': {'dtype': 'uint16', '_FillValue': 9999}})
+        dataset.to_netcdf(img_name, encoding={'num_changes': {'dtype': 'uint16', '_FillValue': 9999}})
 
-		# Set CRS
-		change_map = gdal.Open('new_map.nc')
-		change_map = gdal.Translate('new_map.nc', change_map, outputSRS = 'wkt.txt')
+        # Set CRS
+        change_map = gdal.Open(img_name)
+        change_map = gdal.Translate('change_map_crs.nc', change_map, outputSRS = 'wkt.txt')
+        os.remove(img_name) # Remove change map with no CRS
+        
+        dataset['num_changes'].plot(cmap=plt.cm.OrRd)
+        
+        plt.tight_layout()
+        plt.savefig("change_map_plot.png")
                      
                     
 if __name__ == "__main__":
